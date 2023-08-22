f50fecf1f01eb14d5ce36c44cdb1cce528e8d7a4
Marvin <marvinwanzuita@gmail.com>
2023-08-22 15:55:32 -0300

    projeto criado

---
+++ backend/src/main/java/com/mawit/grafico/GraficoApplication.java
@@ -0,0 +1,13 @@
+package com.mawit.grafico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraficoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraficoApplication.class, args);
+	}
+
+}
