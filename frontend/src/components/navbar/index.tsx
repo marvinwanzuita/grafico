46afcae6ae7f49c2f730ad1858354d1bbbe80135
Marvin <marvinwanzuita@gmail.com>
2023-08-23 12:15:02 -0300

    graficos estaticos

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -5,7 +5,7 @@ function NavBar() {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={MawitLogo} alt="DevSuperior" width="60" />
+          <img src={MawitLogo} alt="DevSuperior" width="40" />
         </nav>
       </div>
     </div>
