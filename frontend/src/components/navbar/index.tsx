b786eb82c13ee874829dc4e00fafb0aacf7806e1
Marvin <marvinwanzuita@gmail.com>
2023-08-24 12:50:52 -0300

    home - navegacao

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import MawitLogo from '../../assets/img/mawit_final.jpg';
 
 function NavBar() {
@@ -5,7 +6,10 @@ function NavBar() {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={MawitLogo} alt="DevSuperior" width="40" />
+          <Link to={'/'}>
+            <img src={MawitLogo} alt="DevSuperior" width="40" />
+          </Link>
+
         </nav>
       </div>
     </div>
