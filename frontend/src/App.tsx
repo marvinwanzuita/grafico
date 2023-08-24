a76ec91483dd266fee7fdcd4fdc838e1fe4408c5
Marvin <marvinwanzuita@gmail.com>
2023-08-24 10:39:52 -0300

    rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,9 @@
-import Footer from "./components/footer";
-import NavBar from "./components/navbar";
-import DataTable from "./components/datatable";
-import BarChart from "components/barchart";
-import DonutChart from "./components/donutchart";
+
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container text-primary px-3">
-        <h1>Dashboard de vendas</h1>
-        <div className="row px-3">
-
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
-            <BarChart/>
-          </div>
-
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
-            <DonutChart/>
-          </div>
-
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
